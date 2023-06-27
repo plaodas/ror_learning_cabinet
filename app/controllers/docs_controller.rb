f083209d6b5910b8bfafc203013127b30b245efe
plaodas <agake@plao-das.com>
2023-06-27 17:52:39 +0900

    add authenticated_root

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,16 +1,19 @@
 class DocsController < ApplicationController
-    before_action :find_doc, only: [:show, :edit, :update, :show, :destory]
+    before_action :find_doc, only: [:show, :edit, :update, :show, :destroy]
 
     def index
-      @docs= Doc.all.order("created_at DESC")
+      # @docs= Doc.all.order("created_at DESC")
+      @docs= Doc.where( user_id: current_user).order("created_at DESC")
     end
 
     def new
-      @doc = Doc.new
+      # @doc = Doc.new
+      @doc= current_user.docs.build 
     end
 
     def create
-      @doc = Doc.new(doc_params)
+      # @doc = Doc.new(doc_params)
+      @doc= current_user.docs.build(doc_params)
       if @doc.save
         redirect_to @doc
       else
@@ -23,15 +26,20 @@ class DocsController < ApplicationController
     end
 
     def show
-      find_doc
     end
 
     def update
+      if @doc.update(doc_params)
+        redirect_to @doc
+      else
+        render 'edit'
+      end
 
     end
 
     def destroy
-
+      @doc.destroy
+      redirect_to docs_path
     end
 
     private
